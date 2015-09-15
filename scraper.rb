112be271f44df02a3702778c089daef7dfeb1ada
Andy Lulham <a.lulham@gmail.com>
2015-09-15 01:16:44 +0100

    Use new column name

--- scraper.rb
+++ scraper.rb
@@ -13,12 +13,12 @@ def members
   morph_api_key = ENV["MORPH_API_KEY"]
   result = RestClient.get morph_api_url, params: {
     key: morph_api_key,
-    query: "select DISTINCT(name) AS wikiname from data"
+    query: "select DISTINCT(wikipedia_name) from data"
   }
   JSON.parse(result, symbolize_names: true)
 end
 
-WikiData.ids_from_pages('en', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
+WikiData.ids_from_pages('en', members.map { |c| c[:wikipedia_name] }).each_with_index do |p, i|
   data = WikiData::Fetcher.new(id: p.last).data rescue nil
   unless data
     warn "No data for #{p}"
