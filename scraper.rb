24fd20793d42d2aa890319a14cca6a52ead9988e
Tony Bowden <tony@tmtm.com>
2016-02-27 16:27:53 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,31 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def members
-  morph_api_url = 'https://api.morph.io/andylolz/gibraltar-wikipedia/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikipedia_name) AS wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-
-WikiData.ids_from_pages('en', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'andylolz/gibraltar-wikipedia', column: 'wikipedia_name')
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
