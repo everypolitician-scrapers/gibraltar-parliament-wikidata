8fecba4541b69788513b11c6db22b5ddc62b58b6
Tony Bowden <tony@tmtm.com>
2017-04-04 14:21:15 +0100

    ALways fetch people already known
    The primary scraper here has broken, and most of the people it had been
    finding don't have a useful Category in Wikipedia, and have no usable
    P39 or equivalent in Wikidata.
    
    So for now, simply assume we want to reload anyone we've previously
    matched.

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country("Gibraltar").lower_house.popolo.persons.map(&:wikidata).compact
+
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'andylolz/gibraltar-wikipedia', column: 'wikipedia_name')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { en: names }, output: false)
